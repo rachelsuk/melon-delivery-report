a16cc1fcfc35512054540f19f79b37ea0280801b
Rachel <rachelsuk97@gmail.com>
2021-01-17 23:46:00 +0000

    refactor code by creating a function and added comments explaining each line of code

--- produce_summary.py
+++ produce_summary.py
@@ -1,43 +1,26 @@
-print("Day 1")
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
-
-
-print("Day 2")
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+def delivery_report(day,file_name):
+    {{{prints daily delivery report on the terminal}}}
+    # print day of delivery on the terminal
+    print(day) 
+    # assigns variable 'the_file' to the contents of the file
+    the_file = open(file_name)
+    # loop through each line of the file
+    for line in the_file:
+        # remove any trailing characters
+        line = line.rstrip()
+        # splits the string into a list of melon name, count and amount
+        words = line.split('|')
+        # accesses items of the list and assigns corresponding variables
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
+        # prints the message
+        print("Delivered {} {}s for total of ${}".format(
+            count, melon, amount))
+    the_file.close()
+# calls the delivery_report function for each day of delivery
+delivery_report("Day 1", "um-deliveries-20140519.txt")
+delivery_report("Day 2", "um-deliveries-20140520.txt")
+delivery_report("Day 3", "um-deliveries-20140521.txt")
 
 
-print("Day 3")
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
